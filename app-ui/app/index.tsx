db3f28f1237d84e6913cc7a0c78867ee59cb6b63
Sally Golding <sally.golding@ufl.edu>
2025-10-26 22:25:39 -0400

    initial expo go project for mobile app ui

---
+++ app-ui/app/index.tsx
@@ -0,0 +1,23 @@
+import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <>
+    <Stack.Screen
+        options={{
+          title: "STEPSMART",
+        }}
+      />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+    </>
+  );
+}
